bb88b860955011797ba4c0ea815af2dfa8303e16
Andrii Oliinyk <andriyjunior14@gmail.com>
2022-06-17 01:14:56 +0300

    feat: add success page and payment methods

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -1,2 +1,4 @@
 export * from './Head';
 export * from './NoScrollLink';
+export * from './PayPalButton';
+export * from './Image';
--- src/components/organisms/Twit/Twit.tsx
+++ src/components/organisms/Twit/Twit.tsx
@@ -1,15 +1,39 @@
-import { FC } from 'react';
+import { motion, useAnimation } from 'framer-motion';
+import { FC, useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
 import { TwitterTweetEmbed } from 'react-twitter-embed';
 import styles from './Twit.module.scss';
 
+const variants = {
+  visible: { opacity: 1, y: 0 },
+  hidden: { opacity: 0, y: 50 },
+};
+
 const Twit: FC = () => {
+  const [ref, inView] = useInView();
+
+  const controls = useAnimation();
+
+  useEffect(() => {
+    if (inView) {
+      controls.start(`visible`);
+    }
+  }, [controls, inView]);
+
   return (
-    <div className={styles.root}>
+    <motion.div
+      ref={ref}
+      variants={variants}
+      initial={`hidden`}
+      animate={controls}
+      transition={{ duration: 1 }}
+      className={styles.root}
+    >
       <TwitterTweetEmbed
         onLoad={(t) => console.log(t)}
         tweetId={`1537023251603496961`}
       />
-    </div>
+    </motion.div>
   );
 };
 
