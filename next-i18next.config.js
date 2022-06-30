02d06efc163b9eda9d9d7ce4ab891842100ddfc8
Andrii Oliinyk <andriyjunior14@gmail.com>
2022-07-01 00:45:00 +0300

    fix: wip

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -4,7 +4,7 @@ module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'ua'],
-    localePath: path.resolve('./public/locales'),
+    localePath: path.resolve('./locales'),
     localeDetection: false,
     fallbackLng: 'en',
   },
